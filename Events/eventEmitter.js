555d8738ffddca88869249e806788ca89d5c8cfb
jkenjayev <69050649+jkenjayev@users.noreply.github.com>
2021-01-08 19:03:34 +0500

    Update eventEmitter.js

--- Events/eventEmitter.js
+++ Events/eventEmitter.js
@@ -1,1 +1,9 @@
 
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+emitter.on("messageLogged", (arg) => {
+  console.log("messageLogged", arg);
+}
+           
+emitter.emit("messageLogged", {id: 1, url: "http://...."});
